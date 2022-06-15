95b65867cb681b496c6e60e6e4f912a58486a033
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-06-14 19:55:17 -0700

    Challenges.

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -1,74 +1,78 @@
 'use strict';
 
 /* ------------------------------------------------------------------------------------------------
+
 CHALLENGE 1 - Review
+
 Write a function named raisedToTheThird that takes in an array of numbers and returns a new array of each of those numbers raised to the 3rd power (hint: look up Math.pow()). Use forEach to solve this problem.
+
 ------------------------------------------------------------------------------------------------ */
 
 const raisedToTheThird = (arr) => {
-  let newArr = [];
-  arr.forEach((num) => newArr.push(Math.pow(num, 3)));
-  return newArr;
+  // Solution code here...
 };
+
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
-Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1.
+
+Write a function named addOne that, given an array of numbers, uses map to return a new array with each value simply incremented by 1. 
 ------------------------------------------------------------------------------------------------ */
 
 const addOne = (arr) => {
-  let newArr = arr.map((num) => num + 1);
-  return newArr;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
+
 Write a function named addQuestion that, given an array of strings, uses map to return a new array containing each string followed by a question mark character.
 ------------------------------------------------------------------------------------------------ */
 
 const addQuestion = (arr) => {
-  let newArr = arr.map((string) => string + '?');
-  return newArr;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
+
 Write a function named forLoopTwoToThe that, given an array of integers as input, iterates over the array and returns a new array. The returned array should contain the result of raising 2 to the power of the original input element.
+
 You may choose to complete this challenge using a for loop, for...in syntax, or for...of syntax.
+
 For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and 2 ^ 3 = 8.
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  let newArr = [];
-  arr.forEach((num) => newArr.push(Math.pow(2, num)));
-  return newArr;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
+
 Write a function named forEachTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 4, but uses forEach instead of a for loop.
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  let newArr = [];
-  arr.forEach((num) => newArr.push(Math.pow(2, num)));
-  return newArr;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
+
 Write a function named mapTwoToThe that produces the same output as your forLoopTwoToThe function from challenge 4 and your forEachTwoToThe function from challenge 5, but uses map instead of a for loop or forEach.
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  let newArr = [];
-  arr.map((num) => newArr.push(Math.pow(2, num)));
-  return newArr;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
+
 Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each element is the result of the `charCodeAt` method on the original array element.
+
 Read the MDN documentation on String.charCodeAt() if necessary.
+
 For example: charCode(['h','i']) returns [104, 105].
 ------------------------------------------------------------------------------------------------ */
 
@@ -78,8 +82,11 @@ const charCode = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8 - Stretch Goal
+
 Write a function that, given an array of numbers as input, uses map to return a new array where each element is either the string "even" or the string "odd", based on each value.
+
 If any element in the array is not a number, the resulting array should have the string "N/A" in its place.
+
 For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 ------------------------------------------------------------------------------------------------ */
 
@@ -89,8 +96,11 @@ const evenOdd = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 9 - Stretch Goal
+
 Use the snorlaxAbilities data, below, for this challenge.
+
 Write a function named extractAbilities that, given the array of abilities, uses map to create an array containing only the ability name.
+
 Note: Because this function is expecting the array of abilities, it will be invoked as:
 extractAbilities(snorlaxAbilities.abilities)
 ------------------------------------------------------------------------------------------------ */
@@ -132,9 +142,13 @@ const extractAbilities = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 10 - Stretch Goal
+
 Use the snorlaxStats data, below, for this challenge.
+
 Write a function named extractStats that, given an array of stats, uses map to return an array of objects containing the stat name and the total.
+
 The total should be the sum of the effort and the baseStat.
+
 Here is an example of a single array element: { name: 'speed', total: 35 }
 ------------------------------------------------------------------------------------------------ */
 
@@ -175,9 +189,13 @@ const extractStats = (arr) => {
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
+
 All the code below will verify that your functions are working to solve the challenges.
+
 DO NOT CHANGE any of the below code.
+
 Run your tests from the console: jest challenges-07.test.js
+
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
