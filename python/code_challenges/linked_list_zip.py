76fa0b952dd6b98a1772babf389a621fa52d51b3
Brentice Loper <brentice@icloud.com>
2022-08-03 13:19:59 -0700

    Code and white board done.

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,20 @@
-def zip_lists(a, b):
+from data_structures.linked_list import LinkedList
+Linked = LinkedList
 
+def zip_lists(a, b):
+    """
+    Given two linked lists, zip them together into one list.
+    """
+    
+    if a is None or b is None:
+        return None
+    else:
+        a_node = a.head
+        b_node = b.head
+        new_list = Linked()
+        while a_node is not None and b_node is not None:
+            new_list.append(a_node.data)
+            new_list.append(b_node.data)
+            a_node = a_node.next
+            b_node = b_node.next
+        return new_list
