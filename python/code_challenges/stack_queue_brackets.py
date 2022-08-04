7a723df5d4443f22a7dc6a2f5e69f499b52110f4
Brentice Loper <brentice@icloud.com>
2022-08-03 22:24:18 -0700

    Code structure made. Still working on tests passing.

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,32 @@
-from data_structures.stack import Queue
+from data_structures.stack import Stack
 
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(string):
+    """
+    Given a string of brackets, return True if the brackets are balanced and False otherwise.
+    """
+    open_stack = Stack()
+    openers = ["{","[","("]
+    closers = ["}","]",")"]
+    pairs = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
+
+    for char in string:
+        if char in openers:
+            open_stack.push(char)
+
+        elif char in closers:
+
+            if open_stack.is_empty():
+
+                return False
+
+            else:
+                if pairs[char] != open_stack.pop():
+                    return False
+
+    return True
+
