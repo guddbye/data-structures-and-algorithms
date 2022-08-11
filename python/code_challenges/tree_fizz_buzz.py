25d1121ef5128b3ca6d669249f34079f138d008d
Brentice Loper <brentice@icloud.com>
2022-08-10 23:48:41 -0700

    Code structure finished. Created white board and algorithm, but not done.

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,35 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree
+from data_structures.queue import Queue
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+
+    def fizz_buzz(num):
+        if num % 15 == 0:
+            return "FizzBuzz"
+
+        elif num % 3 == 0:
+            return "Fizz"
+
+        elif num % 5 == 0:
+            return "Buzz"
+
+        else:
+            return str(num)
+
+    if tree.root is None:
+        return None
+
+    breadth = Queue()
+    breadth.enqueue(tree.root)
+    clone = tree.clone()
+
+
+    while not breadth.is_empty():
+        front = breadth.dequeue()
+        front.value = fizz_buzz(front.value)
+        print(front.children)
+        for child in front.children:
+            breadth.enqueue(child)
+
+    return clone
