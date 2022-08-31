b9ffb11a2d78ec7fc48ccd7d3c3c877ce79ec6c5
Brentice Loper <brentice@icloud.com>
2022-08-30 23:50:33 -0700

    Code done. White board comig.

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,25 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
+def tree_intersection(tree_a, tree_b):
+    table = Hashtable()
+    matches = []
 
-def tree_intersection():
-    pass
+    def walk(root, run):
+        if root is None:
+            return
+        run(root)
+        walk(root.left, run)
+        walk(root.right, run)
+
+    def add_to_hashtable(root):
+        table.set(str(root.value), root.value)
+
+    def add_matches(root):
+        if table.contains(str(root.value)) and root.value not in matches:
+            matches.append(root.value)
+
+    walk(tree_a.root, add_to_hashtable)
+    walk(tree_b.root, add_matches)
+
+    return matches
