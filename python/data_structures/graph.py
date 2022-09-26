95ad94674e7c5ef49f2819da99e67b58d080f95f
Brentice Loper <brentice@icloud.com>
2022-09-26 00:27:17 -0700

    Code challenge done.

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,27 +1,71 @@
-class Graph:
-    def __init__(self):
+from data_structures.queue import Queue
 
-        self._adjacency_list = {}
+class Graph:
+    """
+    add node
+    Arguments: value
+    Returns: The added node
+    Add a node to the graph
+    add edge
+    Arguments: 2 nodes to be connected by the edge, weight (optional)
+    Returns: nothing
+    Adds a new edge between two nodes in the graph
+    If specified, assign a weight to the edge
+    Both nodes should already be in the Graph
+    get nodes
+    Arguments: none
+    Returns all of the nodes in the graph as a collection (set, list, or similar)
+    get neighbors
+    Arguments: node
+    Returns a collection of edges connected to the given node
+    Include the weight of the connection in the returned collection
+    size
+    Arguments: none
+    Returns the total number of nodes in the graph
+    """
+
+    def __init__(self):
+        self.adjacency_list = {}
 
     def add_node(self, value):
         vertex = Vertex(value)
-        self._adjacency_list[vertex] = []
+        self.adjacency_list[vertex] = []
+
         return vertex
 
     def size(self):
-        return len(self._adjacency_list)
+        return len(self.adjacency_list)
 
     def get_nodes(self):
-        return self._adjacency_list.keys()
+        return self.adjacency_list.keys()
 
     def add_edge(self, start_vertex, end_vertex, weight=0):
-        if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
-            raise KeyError()
         edge = Edge(end_vertex, weight)
-        self._adjacency_list[start_vertex].append(edge)
+        if end_vertex not in self.adjacency_list:
+            raise KeyError
+        self.adjacency_list[start_vertex].append(edge)
 
     def get_neighbors(self, vertex):
-        return self._adjacency_list[vertex]
+        return self.adjacency_list[vertex]
+
+    def breadth_first(self, vertex):
+        breadth = Queue()
+        nodes = []
+        visited = []
+
+        breadth.enqueue(vertex)
+        visited.append(vertex)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            nodes.append(front.value)
+
+            for edge in self.get_neighbors(front):
+                if edge.vertex not in visited:
+                    visited.append(edge.vertex)
+                    breadth.enqueue(edge.vertex)
+
+        return nodes
 
 class Vertex:
     def __init__(self, value):
